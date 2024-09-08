628a1d04561bfedfb8359c54adc354ed99133f8a
root <root@GANESH>
2024-09-08 16:29:08 +0530

    zsh config was added

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,17 @@
+alias ld='ls -lap | grep -v /'
+alias lhide='ls -ld .?*'
+alias la='ls -lA'
+
+alias please='sudo !!'
+alias up='python3 -m http.server'
+alias x='exit'
+alias c='clear'
+alias borrow='git clone'
+
+alias -g H='| head' # Pipes output to head (the first part of a file)
+alias -g T='| tail' # Pipes output to tail (the last part of a file)
+alias -g G='| grep' # Pipes output to grep to search for some word
+alias -g L="| less" # Pipes output to less, useful for paging
+alias -g M="| most" # Pipes output to more, useful for paging
+
+alias -s txt='vim'
